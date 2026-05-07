a528f7cb73ceb46e787d87ef5c5bc0ec649cc989
Nikita Yakovlev <nikita.business.me@gmail.com>
2026-05-06 22:01:51 -0500

    Add files via upload

---
+++ adder.py
@@ -0,0 +1,8 @@
+#adder n doubler
+
+
+def add(x, y):
+    return x + y
+
+def double(x):
+    return x*2
---
+++ main.py
@@ -0,0 +1,12 @@
+from adder import add, double
+
+#import adder as a
+#value = a.add(2,2)
+#dbl_value = a.double(value)
+
+
+value = add(2,2)
+print(value)
+
+dbl_value = double(value)
+print(dbl_value)
